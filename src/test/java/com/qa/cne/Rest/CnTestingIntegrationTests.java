8987ca48a68ca0cc7b473c053181b263fd1576e4
KeenanB <k.bratley626@gmail.com>
2020-10-27 15:04:39 +0000

    added comments to clarify code used

--- src/test/java/com/qa/cne/Rest/CnTestingIntegrationTests.java
+++ src/test/java/com/qa/cne/Rest/CnTestingIntegrationTests.java
@@ -1,4 +1,4 @@
-package com.qa.cne;
+package com.qa.cne.Rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.cne.persistence.domain.Badger;
@@ -21,18 +21,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//Launches the Spring context (on a random port to avoid conflicts)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+//Automatically configures the MockMvc object we'll use to perform our tests
 @AutoConfigureMockMvc
+
 //@ActiveProfiles(profiles = "test")
+
+//Executes the schema and data files to reset the Badger table BEFORE each test
 @Sql(scripts = {"classpath:badger-schema.sql",
 				"classpath:badger-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-
-class CnTestingApplicationTests {
+public class CnTestingIntegrationTests {
 
 	@Autowired
+	// Object provided by the Spring testing dependency to allow for easy
+	// integration testing
 	private MockMvc mockMVC;
 
 	@Autowired
+	// Object Spring uses to convert objects sent from/to the @Controller's to and
+	// from JSON
 	private ObjectMapper mapper;
 
 	@Test
@@ -41,25 +50,40 @@ class CnTestingApplicationTests {
 
 	@Test
 	void createsBadger() throws Exception {
+		// Create the request body
 		Badger newBadger = new Badger("fred", 21, "trees");
+		// Convert request body to JSON
 		String testBadgerAsJSON = this.mapper.writeValueAsString(newBadger);
+		// BUILD THE TEST REQUEST
+		// Method = POST
+		// URL = /create
+		// body = testBadgerAsJSON
+		// headers = { "content-type": "application/json" }
 		RequestBuilder request = post("/create").contentType(MediaType.APPLICATION_JSON).content(testBadgerAsJSON);
 
+		// TEST THE STATUS
+		// For a create method it should be 201 (CREATED)
 		ResultMatcher checkStatus = status().is(201);
 
-		Badger savedBadger = new Badger("fred", 21, "trees");
-		savedBadger.setId(2L); // value as 2 because ID 1 should have been inserted with data.sql
-
+		// CREATE expected response body
+		Badger savedBadger = new Badger(2L,"fred", 21, "trees");
+		// Convert it to JSON
 		String expectedAsJSON = this.mapper.writeValueAsString(savedBadger);
+
+		// TEST THE RESPONSE BODY
+		// Should match excpectedAsJSON
 		ResultMatcher checkJSON = content().json(expectedAsJSON);
 
+		// PERFORM THE TEST REQUEST
+		// Sends the POST request we created earlier
+		// Checks the response has the correct status code
+		// Checks the response body matches our expected badger.
 		this.mockMVC.perform(request).andExpect(checkStatus).andExpect(checkJSON);
 
 		MvcResult result = this.mockMVC.perform(request).andExpect(checkStatus).andReturn();
 
 		// to access actual result as an object
 		String reqBody = result.getResponse().getContentAsString();
-
 		Badger badgerResult = this.mapper.readValue(reqBody, Badger.class);
 	}
 
---
+++ src/test/java/com/qa/cne/service/CnTestingUnitTests.java
@@ -0,0 +1,13 @@
+package com.qa.cne.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CnTestingUnitTests {
+
+    @Autowired
+    private BadgerService service;
+
+
+}
